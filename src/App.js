f170c56ed17afdb7effc3fefed2e9caa89e28433
Shymmy W. Garcia <shymmywgarcia@gmail.com>
2019-10-25 13:12:17 -0500

    add solution

--- src/App.js
+++ src/App.js
@@ -3,18 +3,34 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+constructor(){
+  super();
+  this.state={
+   tasks: ['Sacar la ropa','Hacer la cama','Leer un rato'],
+   value:''
+  }
+}
+
+handleChange=(event)=>{
+  this.setState({ value: event.target.value })
+}
+
+handleSubmit=(event)=> {
+  event.preventDefault();
+ this.setState( { tasks: this.state.tasks.concat(this.state.value),
+value:'' });
+}
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+            {this.state.tasks.map((task,index)=><li key={index}>{task}</li>)}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+           <form  onSubmit={this.handleSubmit}>
+             <input value={this.state.value} onChange={this.handleChange} type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
            </form>
         </div>
       </div>
